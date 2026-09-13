14135ae89634ae9a5213b866ff0efca848a2312e
Said Hassan <you@example.com>
2026-09-13 11:32:19 +0300

    Update CORS for VenueYangu domain

--- config/cors.php
+++ config/cors.php
@@ -6,11 +6,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        'http://localhost:5173',
-        'http://127.0.0.1:5173',
-        'https://jade-heliotrope-7ed061.netlify.app',
-    ],
+  'allowed_origins' => [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://venueyangu.netlify.app',
+],
 
     'allowed_origins_patterns' => [],
 
